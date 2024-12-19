c8d6d5cc8a236fa6c30b81a1123a3d0abb3dcc8a
Oscar Alves <osk.alves@gmail.com>
2024-12-19 19:44:10 -0300

    chore: upgrade endpoint and decorations on transaction controller and transaction service

--- apps/hubla-backend/src/transaction/transaction.controller.ts
+++ apps/hubla-backend/src/transaction/transaction.controller.ts
@@ -1,34 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+} from '@nestjs/swagger';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { PagedTransactionsQueryParamsDto } from './dto/paged-transactions-params.dto';
+import { PagedTransactionsResponseDto } from './dto/paged-transactions-response.dto';
+import { TransactionDto } from './dto/transaction.dto';
+import { TransactionService } from './transaction.service';
 
-@Controller('transaction')
+@Controller('transactions')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
+  @ApiOperation({ summary: 'Create a new transaction' })
+  @ApiCreatedResponse({
+    description: 'Transaction created',
+    type: TransactionDto,
+  })
+  @Post()
+  @ApiOperation({ summary: 'Create a new transaction' })
+  @ApiCreatedResponse({
+    description: 'Transaction created',
+    type: TransactionDto,
+  })
+  async create(
+    @Body() createTransactionDto: CreateTransactionDto,
+  ): Promise<TransactionDto> {
     return this.transactionService.create(createTransactionDto);
   }
 
   @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  @ApiOperation({ summary: 'Get paged transactions' })
+  @ApiOkResponse({
+    description: 'Paginated transactions',
+    type: PagedTransactionsResponseDto,
+  })
+  async findAll(
+    @Query() queryParams: PagedTransactionsQueryParamsDto,
+  ): Promise<PagedTransactionsResponseDto> {
+    return this.transactionService.findAll(queryParams);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+  @ApiOperation({ summary: 'Get transaction by ID' })
+  @ApiOkResponse({ description: 'Transaction found', type: TransactionDto })
+  @ApiNotFoundResponse({ description: 'Transaction not found' })
+  async findOne(@Param('id') transactionId: string): Promise<TransactionDto> {
+    return this.transactionService.findOne(transactionId);
   }
 }
--- apps/hubla-backend/src/transaction/transaction.service.ts
+++ apps/hubla-backend/src/transaction/transaction.service.ts
@@ -1,26 +1,85 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { plainToClass } from 'class-transformer';
+import { ResponseHelper } from '../common/helpers/response.helper';
+import { SearchHelper } from '../common/helpers/search.helper';
+import { SortHelper } from '../common/helpers/sort.helper';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { PagedTransactionsQueryParamsDto } from './dto/paged-transactions-params.dto';
+import { PagedTransactionsResponseDto } from './dto/paged-transactions-response.dto';
+import { TransactionDto } from './dto/transaction.dto';
+import { TransactionValidationHelper } from './helpers/transaction-validation.helper';
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  private readonly fieldsToSearch: Prisma.TransactionScalarFieldEnum[] = [
+    'seller',
+    'product',
+    'date',
+  ];
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly transactionValidationHelper: TransactionValidationHelper,
+  ) {}
+
+  async create(
+    createTransactionDto: CreateTransactionDto,
+  ): Promise<TransactionDto> {
+    const transaction = await this.prismaService.transaction.create({
+      data: createTransactionDto,
+    });
+
+    return plainToClass(TransactionDto, transaction);
   }
 
-  findAll() {
-    return `This action returns all transaction`;
+  async findAll({
+    search = '',
+    skip = 0,
+    take = 10,
+    sort = 'date:desc',
+    path = '/transactions',
+  }: PagedTransactionsQueryParamsDto): Promise<PagedTransactionsResponseDto> {
+    const where = SearchHelper.searchBuilder({
+      search,
+      fields: this.fieldsToSearch,
+    });
+
+    const orderBy =
+      SortHelper.parseSortParam<Prisma.TransactionOrderByWithAggregationInput>(
+        sort,
+        [...this.fieldsToSearch, 'date'],
+      );
+
+    const [rows, totalRows] = await this.prismaService.$transaction([
+      this.prismaService.transaction.findMany({
+        where,
+        skip,
+        take,
+        orderBy,
+      }),
+      this.prismaService.transaction.count({ where }),
+    ]);
+
+    return ResponseHelper.pagedResponse<TransactionDto>({
+      path,
+      search,
+      skip,
+      take,
+      sort,
+      rows: rows.map((item) => plainToClass(TransactionDto, item)),
+      totalRows,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
-  }
+  async findOne(id: string) {
+    await this.transactionValidationHelper.validateTransactionExistsById(id);
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
-    return `This action updates a #${id} transaction`;
-  }
+    const transaction = this.prismaService.transaction.findUnique({
+      where: { id },
+    });
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+    return plainToClass(TransactionDto, transaction);
   }
 }

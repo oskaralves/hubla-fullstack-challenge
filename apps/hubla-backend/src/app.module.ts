0af5a5db31aafba08f2ad87086c561820d6b5040
Oscar Alves <osk.alves@gmail.com>
2024-12-19 18:51:43 -0300

    chore: created module transaction with nest cli

--- apps/hubla-backend/src/app.module.ts
+++ apps/hubla-backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })

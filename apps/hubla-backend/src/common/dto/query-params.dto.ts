a33cc7bcaa7802231c18bd33454c88a3abac507b
Oscar Alves <osk.alves@gmail.com>
2024-12-19 19:40:35 -0300

    chore: created query params dto in common

---
+++ apps/hubla-backend/src/common/dto/query-params.dto.ts
@@ -0,0 +1,44 @@
+import { ApiHideProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class QueryParamsDto {
+  @ApiHideProperty()
+  path: string;
+
+  @ApiPropertyOptional({
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  @Transform(({ value }) => decodeURIComponent(value))
+  search: string;
+
+  @ApiPropertyOptional({
+    required: false,
+    example: 10,
+    default: 0,
+  })
+  @IsOptional()
+  @IsInt()
+  @Transform(({ value }) => Number.parseInt(value))
+  skip: number;
+
+  @ApiPropertyOptional({
+    required: false,
+    example: 10,
+    default: 10,
+  })
+  @IsOptional()
+  @IsInt()
+  @Transform(({ value }) => Number.parseInt(value))
+  take: number;
+
+  @ApiPropertyOptional({
+    required: false,
+    example: 'property1:asc,property2:desc',
+  })
+  @IsOptional()
+  @IsString()
+  sort: string;
+}

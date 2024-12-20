93f8558664dde42dbffccc8b16f9d98f981d5eae
Oscar Alves <osk.alves@gmail.com>
2024-12-20 14:12:37 -0300

    config: configured next on frontend app

---
+++ apps/hubla-frontend/next.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    // remotePatterns: [{ hostname: 'storage.googleapis.com' }],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+};
+
+export default nextConfig;

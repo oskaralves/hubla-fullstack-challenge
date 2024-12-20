3f22754a36e686033850a1148e269c3d8bff178c
Oscar Alves <osk.alves@gmail.com>
2024-12-20 01:05:46 -0300

    feat: remove app controller and app service

--- apps/hubla-backend/src/app.module.ts
+++ apps/hubla-backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, TransactionModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    TransactionModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

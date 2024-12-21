8b2b09a8f7aae5688dba0b319b21f5009a2fe6b9
Oscar Alves <osk.alves@gmail.com>
2024-12-21 16:38:49 -0300

    chore: created language context, provider and hook

---
+++ apps/hubla-frontend/src/contexts/AppContext.tsx
@@ -0,0 +1,58 @@
+'use client';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
+
+import { APP_SIDEBAR_EXPANDED } from '@/constants';
+import { setCookie } from 'cookies-next';
+type AppContextProps = {
+  sidebarExpanded: boolean;
+  handleToggleSidebar: () => void;
+};
+
+const initialContext: AppContextProps = {
+  sidebarExpanded: true,
+  handleToggleSidebar: () => undefined,
+};
+
+const AppContext = createContext<AppContextProps>(initialContext);
+
+const AppContextProvider = ({
+  defaultSidebarExpanded: defaultSidebarExpanded,
+  children,
+}: {
+  defaultSidebarExpanded: boolean;
+  children: ReactNode | ReactNode[];
+}) => {
+  const [sidebarExpanded, setSidebarExpanded] = useState<
+    AppContextProps['sidebarExpanded']
+  >(defaultSidebarExpanded);
+
+  const handleToggleSidebar = useCallback(() => {
+    setSidebarExpanded((prevState) => !prevState);
+  }, [setSidebarExpanded]);
+
+  useEffect(() => {
+    setCookie(APP_SIDEBAR_EXPANDED, sidebarExpanded);
+  }, [sidebarExpanded, defaultSidebarExpanded]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        sidebarExpanded,
+        handleToggleSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useAppContext = () => useContext(AppContext);
+
+export { AppContext, AppContextProvider, useAppContext };
---
+++ apps/hubla-frontend/src/contexts/language-context.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { createContext, useContext, useState, useTransition } from 'react';
+import { Country } from 'react-phone-number-input';
+
+export type Locale = 'pt-BR' | 'en-US';
+export const countries: Record<Locale, Country> = {
+  'pt-BR': 'BR',
+  'en-US': 'US',
+};
+type LanguageContextProps = {
+  locale: Locale;
+  setLocale: (locale: Locale) => void;
+  isPending: boolean;
+  countries: Record<Locale, Country>;
+};
+
+const LanguageContext = createContext<LanguageContextProps | undefined>(
+  undefined
+);
+
+export const LanguageProvider = ({
+  children,
+  initialLocale,
+}: {
+  children: React.ReactNode;
+  initialLocale: Locale;
+}) => {
+  const [locale, setLocale] = useState<Locale>(initialLocale);
+  const [isPending, startTransition] = useTransition();
+
+  const router = useRouter();
+
+  const handleSetLocale = async (newLocale: Locale) => {
+    startTransition(async () => {
+      await fetch('/api/set-locale', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ locale: newLocale }),
+      });
+
+      // Atualiza o estado do idioma
+      setLocale(newLocale);
+
+      // Usando o router.replace para forçar o re-render sem recarregar a página
+      router.refresh();
+    });
+  };
+
+  return (
+    <LanguageContext.Provider
+      value={{ locale, setLocale: handleSetLocale, countries, isPending }}
+    >
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
+};

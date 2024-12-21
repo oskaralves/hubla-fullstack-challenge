001c4a298b2fada7d329c3160ee7189d1fbe8de0
Oscar Alves <osk.alves@gmail.com>
2024-12-21 16:37:39 -0300

    chore: created general constants

---
+++ apps/hubla-frontend/src/constants/index.ts
@@ -0,0 +1,21 @@
+export const menuItemIconProps = {
+  width: 22,
+  height: 22,
+  strokeWidth: 1.5,
+};
+
+export const PREFIX_KEY =
+  process.env.NODE_ENV === "development" ? "hubla.__dev__" : "hubla";
+
+//  Keys of local storage or cookie
+export const APP_TITLE = `${PREFIX_KEY !== "hubla" ? "[DEV] " : ""}Hubla - Full Stack Challenge`;
+export const APP_LOCALE_KEY = `${PREFIX_KEY}.app-locale`;
+export const APP_SIDEBAR_EXPANDED = `${PREFIX_KEY}.sidebar-expanded`;
+
+// Theme
+
+// Pagination
+export const ROWS_PER_PAGE_OPTIONS = [5, 10, 25, 50, 100];
+
+// Default Value of Filters
+export const DEFAULT_ITEMS_PER_PAGE = 10;

ef10023ec3a91185241d21ef83bba4f6b580a146
Oscar Alves <osk.alves@gmail.com>
2024-12-22 12:08:00 -0300

    chore: added item on menu in navigation

--- apps/hubla-frontend/src/navigation/admin.tsx
+++ apps/hubla-frontend/src/navigation/admin.tsx
@@ -1,18 +1,21 @@
 import { menuItemIconProps } from "@/constants";
 import { Locale } from "@/contexts/language-context";
 import { getDictionary } from "@/dictionaries";
-import { DocumentAttachmentIcon, TransactionIcon } from "hugeicons-react";
+import {
+  DocumentAttachmentIcon,
+  ManagerIcon,
+  TransactionIcon,
+} from "hugeicons-react";
 import { IMenuItem } from "./types";
 import {
   ADMIN_TRANSACTION_IMPORT_URL,
   ADMIN_TRANSACTION_LIST_URL,
+  ADMIN_TRANSACTION_SELLER_BALANCES_URL,
 } from "./urls";
 
 export const admin = (locale: Locale): IMenuItem => {
-  const { ADMINISTRATION, TRANSACTIONS, IMPORT_TRANSACTIONS } = getDictionary(
-    locale,
-    "navigation"
-  );
+  const { ADMINISTRATION, TRANSACTIONS, IMPORT_TRANSACTIONS, BALANCES } =
+    getDictionary(locale, "navigation");
   return {
     id: "admin-group",
     title: ADMINISTRATION,
@@ -26,11 +29,18 @@ export const admin = (locale: Locale): IMenuItem => {
         icon: <TransactionIcon {...menuItemIconProps} />,
       },
       {
-        id: "admin-import-transactions",
+        id: "admin-transactions-import",
         title: IMPORT_TRANSACTIONS,
         type: "item",
         url: ADMIN_TRANSACTION_IMPORT_URL,
         icon: <DocumentAttachmentIcon {...menuItemIconProps} />,
+      },
+      {
+        id: "admin-transactions-balance",
+        title: BALANCES,
+        type: "item",
+        url: ADMIN_TRANSACTION_SELLER_BALANCES_URL,
+        icon: <ManagerIcon {...menuItemIconProps} />,
       },
     ],
   };
--- apps/hubla-frontend/src/navigation/urls.ts
+++ apps/hubla-frontend/src/navigation/urls.ts
@@ -3,3 +3,4 @@ export const BASE_ROUTE = "/";
 // Transaction
 export const ADMIN_TRANSACTION_LIST_URL = `/transactions`;
 export const ADMIN_TRANSACTION_IMPORT_URL = `/transactions/import`;
+export const ADMIN_TRANSACTION_SELLER_BALANCES_URL = `/transactions/seller-balances`;

83806a148ab50dcc27bff69b1d34d351bb5bb296
Oscar Alves <osk.alves@gmail.com>
2024-12-23 12:25:45 -0300

    chore: auth, role, user, permission and cache modules on app module

--- apps/hubla-backend/src/app.module.ts
+++ apps/hubla-backend/src/app.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { CacheModule } from './cache/cache.module';
+import { PermissionModule } from './permission/permission.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { RoleModule } from './role/role.module';
 import { TransactionTypeModule } from './transaction-type/transaction-type.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule,
     PrismaModule,
+    AuthModule,
     TransactionModule,
     TransactionTypeModule,
+    RoleModule,
+    UserModule,
+    PermissionModule,
   ],
   controllers: [],
   providers: [],
